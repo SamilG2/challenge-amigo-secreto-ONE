a0a1628897ade236f4715a16b9c2e11d940b47a3
SamilG2 <devsamilg@gmail.com>
2025-03-15 16:46:10 -0500

    Implementación de función actualizarAmigos, index y style

--- app.js
+++ app.js
@@ -11,23 +11,44 @@ function agregarAmigo() {
     if(nombreAmigo!== '' && /^[a-zA-Z\s]+$/.test(nombreAmigo)){
         amigos.push(nombreAmigo);
         console.log(amigos);
-        //Función para limpiar campo de entrada
+        //Limpia el campo de entrada
         document.getElementById("amigo").value = "";
+        //Función para visualizar los amigos en pantalla
+        actualizarAmigos();
 
     //Si en caso no se cumple la validación, mostraremos un alert
     } else {
-        alert("Por favor, ingresa un nombre válido.");
+        alert("Por favor, inserte un nombre.");
     }
 
 }
 
+//Función para sortear amigo
 function sortearAmigo() {
-    if(amigos !=0) {
+    //Si la lista de amigos no esta vacia
+    if(amigos.length !== 0) {
+        //Sorteamos al amigo secreto
         let amigoSecreto = amigos[Math.floor(Math.random() * amigos.length)];
+        //Mostramos el amigo secreto
         let resultado = document.getElementById("resultado");
         resultado.innerHTML = "El amigo secreto es: " + amigoSecreto;
-        amigos = [];        
+        document.getElementById("listaAmigos").innerHTML = "";
+        amigos = [];      
+      //Si en caso no se cumple la validación, mostraremos un alert 
     } else {
         alert("No hay amigos para sortear.");
+    }
+}
+
+//Función para actualizar la lista de amigos
+function actualizarAmigos() {
+    let amigosList = document.getElementById("listaAmigos");
+    //Limpiamos la lista para evitar duplicados
+    amigosList.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        //Usamos el elemento <li> para los nombres
+        let amigoItem = document.createElement("li");
+        amigoItem.textContent = amigos[i];
+        amigosList.appendChild(amigoItem);
     }
 }
